dc23c840929f802405d5a326805fda668b13bd9c
aharris52 <aharris@mail.greenriver.edu>
2020-05-13 18:06:24 -0400

    added views and htaccess

--- index.php
+++ index.php
@@ -1,25 +1,14 @@
 <?php
-/*
 //Turn on error reporting
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-//Require the autoload file
-require_once('vendor/autoload.php');
-//Create an instance of the Base class
-$f3 = Base::instance();
-//Define a default route
-$f3->route('GET /', function() {
-    echo '<h1>Hello, world!</h1>';
-});
-//Run fat free
-$f3->run();*/
-// turn on error reporting
 ini_set('display_errors',1);
 error_reporting(E_ALL);
-// require the autoload file
+
+// require the autoload file/s
 require_once('vendor/autoload.php');
-// create an instance of the base class
+
+// create an instance of the F3 base class
 $f3 = Base::instance();
+
 // define a default route
 $f3->route('GET /', function(){
     //echo '<h1>Hello world!</h1>';
