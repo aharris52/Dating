3e87fe18149670d54b0568900c5da246035a214c
aharris52 <aharris@mail.greenriver.edu>
2020-05-14 01:15:47 -0400

    .htaccess and index.php profile route

--- index.php
+++ index.php
@@ -1,31 +1,45 @@
 <?php
-/*
-//Turn on error reporting
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-//Require the autoload file
-require_once('vendor/autoload.php');
-//Create an instance of the Base class
-$f3 = Base::instance();
-//Define a default route
-$f3->route('GET /', function() {
-    echo '<h1>Hello, world!</h1>';
-});
-//Run fat free
-$f3->run();*/
 // turn on error reporting
 ini_set('display_errors',1);
 error_reporting(E_ALL);
+
 // require the autoload file
 require_once('vendor/autoload.php');
+
 // create an instance of the base class
 $f3 = Base::instance();
+
 // define a default route
 $f3->route('GET /', function(){
+
+    //troubleshooting
     echo '<h1>Hello world!</h1>';
+
     $view = new Template();
-    echo $view->render('view/home2.html');
+
+    //troubleshooting
+    echo $view->render('views/home2.html');
     echo '<h1>Hello world!</h1>';
 });
+// close default
+
+// personal info route
+$f3->route('GET|POST /personal-information', function($f3){
+
+    $view = new Template();
+    echo $view->render('views/personal-information.html');
+
+});
+
+// profile route
+$f3->route('GET|POST /profile', function($f3){
+
+    //troubleshooting
+    echo '<h1>Hello world!</h1>';
+    //$view = new Template();
+    //echo $view->render('views/profile.html');
+
+});
+
 // run fat free
 $f3->run();
