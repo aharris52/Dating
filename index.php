27a3525c5021a39c13f852c485b4a4275632aa1d
aharris52 <aharris@mail.greenriver.edu>
2020-05-27 21:48:37 -0400

    added index routing

--- index.php
+++ index.php
@@ -35,19 +35,15 @@ $f3->route('GET|POST /personal-information', function($f3){
         //var_dump($_POST);
 
         //ensure name fields filled-out
-        if (!validName($_POST['first'])) {
+        if (!validName($_POST['firstName'])) {
             $f3->set('errors["first"]', "Provide a valid first name please");
         }
-        if (!validName($_POST['last'])) {
-            $f3->set('errors["last"]', "Provide a last name please");
+        if (!validName($_POST['lastName'])) {
+            $f3->set('errors["last"]', "Provide a valid last name please");
         }
         //Ensure applicant is >18yo
         if (!validAge($_POST['age'])) {
             $f3->set('errors["age"]', "Invalid age");
-        }
-        //must select a gender
-        if ($gender == "") {
-            $f3->set('errors["gender"]', "Please select a gender");
         }
         //must provide phone#
         if (!validPhone($_POST['phone'])) {
@@ -58,8 +54,8 @@ $f3->route('GET|POST /personal-information', function($f3){
         if (empty($f3->get('errors'))) {
 
             //Store the data in the session array
-            $_SESSION['first'] = $_POST['first'];
-            $_SESSION['last'] = $_POST['last'];
+            $_SESSION['first'] = $_POST['firstName'];
+            $_SESSION['last'] = $_POST['lastName'];
             $_SESSION['age'] = $_POST['age'];
             $_SESSION['gender'] = $_POST['gender'];
             $_SESSION['phone'] = $_POST['phone'];
@@ -69,8 +65,8 @@ $f3->route('GET|POST /personal-information', function($f3){
         }
     }
 
-    $f3->set('first', $_POST['first']);
-    $f3->set('last', $_POST['last']);
+    $f3->set('first', $_POST['firstName']);
+    $f3->set('last', $_POST['lastName']);
     $f3->set('age', $_POST['age']);
     $f3->set('gender', $gender);
     $f3->set('phone', $_POST['phone']);
@@ -84,31 +80,97 @@ $f3->route('GET|POST /personal-information', function($f3){
 // profile route
 $f3->route('GET|POST /profile', function($f3){
 
+    //get the States and Genders
+    $inputState = getStates();
+    $seeking = getGender();
+
+    //If the form has been submitted
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        //var_dump($_SESSION);
+
+        //ensure they provide a valid email
+        if(!validEmail($_POST['email'])) {
+            $f3->set('errors["email"]', "Invalid email");
+        }
+        //make sure they filled it out
+        if (empty($f3->get('errors'))) {
+            //Store the data from post to session array
+            $_SESSION['email'] = $_POST['email'];
+            $_SESSION['state'] = $_POST['state'];
+            $_SESSION['seeking'] = $_POST['seeking'];
+            $_SESSION['bio'] = $_POST['bio'];
+
+            //Redirect to interests page
+            $f3->reroute('/interests');
+        }
+    }
+    //retrieve from the hive
+    $f3->set('selected', $_POST['state']);
+    $f3->set('seeks', $_POST['seeking']);
+    //store
+    $f3->set('email', $_POST['email']);
+    $f3->set('state', $inputState);
+    $f3->set('seeking', $seeking);
+
+
+    "<pre>" . var_dump($_POST). "</pre>";
+    "<pre>" . var_dump($_SESSION). "</pre>";
+
     //troubleshooting
     //echo '<h1>Hello world!</h1>';
     $view = new Template();
     echo $view->render('views/profile.html');
-
-    echo "<pre>" . "post" . var_dump($_POST). "</pre>";
-    echo "<pre>" . "session" . var_dump($_POST). "</pre>";
 
 });
 
 // interest route
 $f3->route('GET|POST /interests', function($f3){
 
+    $indoor = getInDoor();
+    $outdoor = getOutDoor();
+
+    //If the form has been submitted
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        //var_dump($_POST);
+
+        if(!validIndoor($_POST['indoor'])) {
+            $f3->set('errors["indoor"]', "Please select an indoor activity/ies");
+        }
+
+        if(!validOutdoor($_POST['outdoor'])) {
+            $f3->set('errors["outdoor"]', "Please select an indoor activity/ies");
+        }
+
+        if (empty($f3->get('errors'))) {
+            //Store the data in the session array
+            $_SESSION['indoor'] = $_POST['indoor'];
+            $_SESSION['outdoor'] = $_POST['outdoor'];
+
+            //Redirect to summary page
+            $f3->reroute('/summary');
+        }
+    }
+
+    $f3->set('selected', $_POST['indoor']);
+    $f3->set('selectedOut', $_POST['outdoor']);
+    $f3->set('indoor', $indoor);
+    $f3->set('outdoor', $outdoor);
+
     //troubleshooting
-    echo '<h1>Hello world!</h1>';
+    //echo '<h1>Hello world!</h1>';
+    "<pre>" . var_dump($_POST). "</pre>";
+    "<pre>" . var_dump($_SESSION). "</pre>";
     $view = new Template();
     echo $view->render('views/interests.html');
 
 });
 
-// interest route
+// summary route
 $f3->route('GET|POST /summary', function($f3){
 
     //troubleshooting
-    echo '<h1>Hello world!</h1>';
+    //echo '<h1>Hello world!</h1>';
+    var_dump($_SESSION);
     $view = new Template();
     echo $view->render('views/summary.html');
 
